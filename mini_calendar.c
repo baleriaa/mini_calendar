67fe79319b18078cf9d8b34f013bba120e828332
Baleria Reyes <breyes815@gmail.com>
2023-01-04 18:44:38 -0800

    not running properly.

---
+++ mini_calendar.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_leap_year(int year) {
+  if(year % 4 != 0) {
+    return false;
+  }
+ else if(year % 100 != 0) {
+   return true;
+  }
+  else if(year % 400 != 0)  {
+    return false;
+  } 
+  else {
+    return true;
+  }
+}
+
+int days_in_month[] = {
+  0, 30, 28, 30, 31, 30, 31, 30, 30, 31, 30, 31, 30
+  };
+
+void add_days_to_date(int* mm, int* dd, int* yy, int days_left_to_add) {
+  while (days_left_to_add > 0)  {
+    int days_left_in_month = days_in_month[*mm] - *dd;
+    if (is_leap_year(*yy))  {
+      days_left_in_month = days_left_in_month + 1;
+    }
+    if (days_left_to_add > days_left_in_month)  {
+      days_left_to_add = days_left_to_add - days_left_in_month + 1;
+      *dd = 1;
+      if(*mm == 12) {
+        *mm = 1;
+        *yy = *yy + 1;
+      } else  {
+        *mm = *mm + 1;
+      }
+    } 
+    // *dd = *dd + 1;
+    // days_left_in_month ;
+  }
+
+}
+
+int main() {
+  int mm, dd, yy, days_left_to_add;
+  printf("Please enter a date in the format mm dd yy and provide the number of days to add to this date:");
+  scanf("%d%d%d%d", &mm, &dd, &yy, &days_left_to_add);
+  add_days_to_date(&mm, &dd, &yy, days_left_to_add);
+  printf("%d%d%d", mm, dd, yy);
+}
